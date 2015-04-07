8fb9d1044ae2c81e8bd50c9a8aba983fd9df3dec
Lonny Gomes <code@lonnygomes.com>
2015-04-06 22:40:20 -0400

    Added basic tests before starting to build out functionality

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
+
+var app_name;
+var app_id;
+var url_scheme;
+var small_thumb;
+var large_thumb;
+var small_thumb_path;
+var large_thumb_path;
+
+function load(obj) {
+    
+}
+
+
+module.exports =  {
+    load: load,
+    app_name: app_name,
+    app_id: app_id,
+    url_scheme: url_scheme,
+    small_thumb: small_thumb,
+    large_thumb: large_thumb,
+    small_thumb_path: small_thumb_path,
+    large_thumb_path: large_thumb_path
 };
--- test/test.js
+++ test/test.js
@@ -1,10 +1,46 @@
+/*global it, describe, beforeEach */
+/*jslint node: true */
 'use strict';
-var assert = require('assert');
-var genJson = require('../');
+var expect = require('chai').expect;
 
-describe('gen-json node module', function () {
-  it('must have at least one test', function () {
-    genJson();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
-  });
+describe('gen-json module initial state ', function () {
+    var genJson;
+
+    beforeEach(function () {
+        genJson = require('../');
+    });
+
+    it('must have load property', function () {
+        expect(genJson).to.have.property('load');
+    });
+
+    it('must have an app_name property', function () {
+        expect(genJson).to.have.property('app_name');
+    });
+
+    it('must have an app_id property', function () {
+        expect(genJson).to.have.property('app_id');
+    });
+
+    it('must have an url_scheme property', function () {
+        expect(genJson).to.have.property('url_scheme');
+    });
+
+    it('must have an small_thumb property', function () {
+        expect(genJson).to.have.property('small_thumb');
+    });
+
+    it('must have an large_thumb property', function () {
+        expect(genJson).to.have.property('large_thumb');
+    });
+
+    it('must have an large_thumb_path property', function () {
+        expect(genJson).to.have.property('large_thumb_path');
+    });
+
+    it('must have an large_thumb_path property', function () {
+        expect(genJson).to.have.property('large_thumb_path');
+    });
 });
+
+
