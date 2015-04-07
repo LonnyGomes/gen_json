5ecc57371e2e702d8814d16cd82c7f36fb8d8bbf
Lonny Gomes <code@lonnygomes.com>
2015-04-06 23:22:05 -0400

    Added more tests, converted properties to getters & setters

--- index.js
+++ index.js
@@ -9,17 +9,52 @@ var small_thumb_path;
 var large_thumb_path;
 
 function load(obj) {
-    
+    if (obj.app_name && obj.app_id && obj.url_scheme &&
+        obj.small_thumb_path && obj.large_thumb_path) {
+
+        app_name = obj.app_name;
+        app_id = obj.app_id;
+        url_scheme = obj.url_scheme;
+        small_thumb_path = obj.small_thumb_path;
+        large_thumb_path = obj.large_thumb_path;
+    } else  {
+        throw new Error('Missing required parameters!');
+    }
 }
 
 
 module.exports =  {
     load: load,
-    app_name: app_name,
-    app_id: app_id,
-    url_scheme: url_scheme,
+    get app_name() {
+        return app_name;
+    },
+    set app_name(value) {
+        app_name = value;
+    },
+    get app_id()  {
+        return app_id;
+    },
+    set app_id(value) {
+        app_id = value;
+    },
+    get url_scheme() {
+        return url_scheme;
+    },
+    set url_scheme(value) {
+        url_scheme = value;
+    },
+    get small_thumb_path() {
+        return small_thumb_path;
+    },
+    set small_thumb_path(value) {
+        small_thumb_path = value;
+    },
+    get large_thumb_path() {
+        return large_thumb_path;
+    },
+    set large_thumb_path(value) {
+        large_thumb_path = value;
+    },
     small_thumb: small_thumb,
-    large_thumb: large_thumb,
-    small_thumb_path: small_thumb_path,
-    large_thumb_path: large_thumb_path
+    large_thumb: large_thumb
 };
--- test/test.js
+++ test/test.js
@@ -43,4 +43,87 @@ describe('gen-json module initial state ', function () {
     });
 });
 
+describe('load', function () {
+    var genJson,
+        sampleParams;
 
+    beforeEach(function () {
+        sampleParams = {
+            app_name: "test_name",
+            app_id: "test_id",
+            url_scheme: "testscheme",
+            small_thumb_path: "assets/small-thumb.png",
+            large_thumb_path: "assets/large-thumb.png"
+        };
+
+        genJson = require('../');
+    });
+
+    it ('should not throw an error if all required fields are supplied', function () {
+        var load = function() {
+            genJson.load(sampleParams);
+        };
+        expect(load).to.not.throw(Error);
+    });
+
+    it('should throw an error if app_name is not supplied', function () {
+        expect(function () {
+            delete sampleParams.app_name;
+            genJson.load(sampleParams);
+        }).to.throw(Error);
+    });
+
+    it('should throw an error if app_id is not supplied', function () {
+        expect(function () {
+            delete sampleParams.app_id;
+            genJson.load(sampleParams);
+        }).to.throw(Error);
+    });
+
+    it('should throw an error if url_scheme is not supplied', function () {
+        expect(function () {
+            delete sampleParams.url_scheme;
+            genJson.load(sampleParams);
+        }).to.throw(Error);
+    });
+
+    it('should throw an error if small_thumb_path is not supplied', function () {
+        expect(function () {
+            delete sampleParams.url_scheme;
+            genJson.load(sampleParams);
+        }).to.throw(Error);
+    });
+
+    it('should throw an error if large_thumb_path is not supplied', function () {
+        expect(function () {
+            delete sampleParams.large_thumb_path;
+            genJson.load(sampleParams);
+        }).to.throw(Error);
+    });
+
+    it('should populate app_name property', function () {
+        genJson.load(sampleParams);
+        expect(genJson.app_name).equals(sampleParams.app_name);
+    });
+
+    it('should populate app_id property', function () {
+        genJson.load(sampleParams);
+        expect(genJson.app_id).equals(sampleParams.app_id);
+    });
+
+    it('should populate url_scheme property', function () {
+        genJson.load(sampleParams);
+        expect(genJson.url_scheme).equals(sampleParams.url_scheme);
+    });
+
+    it('should populate small_thumb_path property', function () {
+        genJson.load(sampleParams);
+        expect(genJson.small_thumb_path).equals(sampleParams.small_thumb_path);
+    });
+
+    it('should populate large_thumb_path property', function () {
+        genJson.load(sampleParams);
+        expect(genJson.large_thumb_path).equals(sampleParams.large_thumb_path);
+    });
+
+});
